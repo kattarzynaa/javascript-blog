e552bc2db63f7396f1ef28a8e41aedb839fe34b1
Kasia Wetula <wetula.kasia@gmail.com>
2023-03-13 16:09:04 +0100

    ADD class active to clicked link

--- js/script.js
+++ js/script.js
@@ -9,21 +9,28 @@
     console.log('Link was clicked!');
     console.log(event);
 
-    /* remove class 'active' from all article links */
+    const clickedElement = this;
+
+    /* [DONE] remove class 'active' from all article links */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
      for(let activeLink of activeLinks) {
         activeLink.classList.remove('active');
      }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    console.log('clickedElement:', clickedElement);
+
+    clickedElement.classList.add('active');
+    //console.log('clickedElement (with plus):' + clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .active');
     for(let activeArticle of activeArticles) {
        activeArticle.classList.remove('active');
-    } //TO NIE DZIAŁA JESZCZE :((
+    } 
 
     /* get 'href' attribute from the clicked link */
 
